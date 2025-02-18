6f33b4dd72434713a9dcac3c98dca70c67eb8f0a
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2025-02-17 23:42:57 -0800

    update

--- Practice/1.cpp
+++ Practice/1.cpp
@@ -1,3 +1,8 @@
+/**
+ * Author: C0ldSmi1e
+ * Created Time: 02/16/2025 09:21:35 PM
+**/
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -15,17 +20,39 @@ int main() {
   int T;
   cin >> T;
   while (T--) {
-    string s;
-    cin >> s;
-    int n = (int) s.size();
-    int ans = (int) s.size();
+    int n, m;
+    cin >> n >> m;
+    vector<array<int, 2>> a(n);
+    for (int i = 0; i < n; i++) {
+      cin >> a[i][0];
+    }
+    int x;
+    cin >> x;
+    for (int i = 0; i < n; i++) {
+      a[i][1] = x - a[i][0];
+    }
+    vector<vector<bool>> dp(n + 1, vector<bool>(2));
+    dp[0][0] = true;
+    dp[0][1] = true;
     for (int i = 0; i < n - 1; i++) {
-      if (s[i] == s[i + 1]) {
-        ans = 1;
-        break;
+      if (dp[i][0]) {
+        if (a[i + 1][0] >= a[i][0]) {
+          dp[i + 1][0] = true;
+        }
+        if (a[i + 1][1] >= a[i][0]) {
+          dp[i + 1][1] = true;
+        }
+      }
+      if (dp[i][1]) {
+        if (a[i + 1][0] >= a[i][1]) {
+          dp[i + 1][0] = true;
+        }
+        if (a[i + 1][1] >= a[i][1]) {
+          dp[i + 1][1] = true;
+        }
       }
     }
-    cout << ans << '\n';
+    cout << ((dp[n - 1][0] || dp[n - 1][1]) ? "YES\n" : "NO\n");
   }
   return 0;
 }
