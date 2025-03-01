ac1cd2a1e65628744e9f04198aefcf35c0113813
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2025-02-28 22:19:03 -0800

    update

--- Practice/1.cpp
+++ Practice/1.cpp
@@ -1,6 +1,6 @@
 /**
  * Author: C0ldSmi1e
- * Created Time: 02/23/2025 10:31:08 AM
+ * Created Time: 02/27/2025 10:44:34 PM
 **/
 
 #include <bits/stdc++.h>
@@ -17,69 +17,25 @@ int main() {
   cin.tie(nullptr)->sync_with_stdio(false);
   cout << fixed << setprecision(10);
 
-  int n;
-  cin >> n;
-  vector<pair<int, int>> a(n);
-  auto GetL = [&](string s) -> int {
-    int cur = 0;
-    int mn = 0;
-    for (auto& c : s) {
-      if (c == '(') {
-        cur += 1;
-      } else {
-        cur -= 1;
-      }
-      mn = min(mn, cur);
+  int T;
+  cin >> T;
+  while (T--) {
+    int n = 4;
+    vector<int> a(n);
+    for (auto& u : a) {
+      cin >> u;
+      u %= 12;
     }
-    return mn;
-  };
-  auto GetR = [&](string s) -> int {
-    int cur = 0;
-    int mn = 0;
-    reverse(s.begin(), s.end());
-    for (auto& c : s) {
-      if (c == ')') {
-        cur += 1;
-      } else {
-        cur -= 1;
-      }
-      mn = min(mn, cur);
-    }
-    return mn;
-  };
-  int ans = 0;
-  int good = 0;
-  map<int, int> L, R;
-  for (int i = 0; i < n; i++) {
-    string s;
-    cin >> s;
-    int l = abs(GetL(s));
-    int r = abs(GetR(s));
-    a[i] = make_pair(l, r);
-    debug(l, r);
-    if (l == 0 && r == 0) {
-      good += 1;
-      continue;
-    }
-    if (l == 0) {
-      if (R[r] > 0) {
-        R[r] -= 1;
-        ans += 1;
-      } else {
-        L[r] += 1;
-      }
-      continue;
-    }
-    if (r == 0) {
-      if (L[l] > 0) {
-        L[l] -= 1;
-        ans += 1;
-      } else {
-        R[l] += 1;
-      }
+    vector<int> order(n);
+    iota(order.begin(), order.end(), 0);
+    sort(order.begin(), order.end(), [&](int i, int j) {
+      return a[i] < a[j];
+    });
+    if (abs(order[0] - order[2]) == 1 && !((order[0] == 1 && order[2] == 2) || (order[0] == 2 && order[2] == 1))) {
+      cout << "YES\n";
+    } else {
+      cout << "NO\n";
     }
   }
-  ans += good / 2;
-  cout << ans << '\n';
   return 0;
 }
