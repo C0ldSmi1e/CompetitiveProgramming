96907a81a270c3b56b8a4c227f600e9a5c9e47ce
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2025-03-02 20:22:00 -0800

    update

--- Practice/1.cpp
+++ Practice/1.cpp
@@ -1,6 +1,6 @@
 /**
  * Author: C0ldSmi1e
- * Created Time: 02/27/2025 10:44:34 PM
+ * Created Time: 03/02/2025 06:51:24 PM
 **/
 
 #include <bits/stdc++.h>
@@ -20,22 +20,17 @@ int main() {
   int T;
   cin >> T;
   while (T--) {
-    int n = 4;
-    vector<int> a(n);
-    for (auto& u : a) {
-      cin >> u;
-      u %= 12;
+    int x, y;
+    cin >> x >> y;
+    int ans = 0;
+    int cnt = (y + 1) / 2;
+    int empty = cnt * 3 * 5 - y * 4;
+    ans = cnt;
+    if (empty < x) {
+      x -= empty;
+      ans += (x + 14) / 15;
     }
-    vector<int> order(n);
-    iota(order.begin(), order.end(), 0);
-    sort(order.begin(), order.end(), [&](int i, int j) {
-      return a[i] < a[j];
-    });
-    if (abs(order[0] - order[2]) == 1 && !((order[0] == 1 && order[2] == 2) || (order[0] == 2 && order[2] == 1))) {
-      cout << "YES\n";
-    } else {
-      cout << "NO\n";
-    }
+    cout << ans << '\n';
   }
   return 0;
 }
