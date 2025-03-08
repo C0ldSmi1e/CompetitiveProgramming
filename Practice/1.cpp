6c7b8ac8d970f78ff79b68fb31428284d0c0ef7b
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2025-03-08 08:20:19 -0800

    update

--- Practice/1.cpp
+++ Practice/1.cpp
@@ -1,6 +1,6 @@
 /**
  * Author: C0ldSmi1e
- * Created Time: 03/04/2025 09:42:08 AM
+ * Created Time: 03/08/2025 06:26:25 AM
 **/
 
 #include <bits/stdc++.h>
@@ -17,100 +17,42 @@ int main() {
   cin.tie(nullptr)->sync_with_stdio(false);
   cout << fixed << setprecision(10);
 
-  int T;
-  cin >> T;
-  while (T--) {
-    int n, m;
-    cin >> n >> m;
-    vector<string> g(n);
-    for (auto& s : g) {
-      cin >> s;
-    }
-    vector<vector<bool>> st(n, vector<bool>(m));
-    for (int i = 0; i < n; i++) {
-      if (g[i][0] == 'L') {
-        st[i][0] = true;
-      }
-      if (g[i][m - 1] == 'R') {
-        st[i][m - 1] = true;
-      }
-    }
-    for (int j = 0; j < m; j++) {
-      if (g[0][j] == 'U') {
-        st[0][j] = true;
-      }
-      if (g[n - 1][j] == 'D') {
-        st[n - 1][j] = true;
-      }
-    }
-    vector<pair<int, int>> q;
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        if (st[i][j]) {
-          q.emplace_back(make_pair(i, j));
-        }
-      }
-    }
-    map<char, pair<int, int>> mp;
-    mp['L'] = make_pair(0, -1);
-    mp['R'] = make_pair(0, 1);
-    mp['U'] = make_pair(-1, 0);
-    mp['D'] = make_pair(1, 0);
-    const int dx[4] = {0, 0, -1, 1};
-    const int dy[4] = {1, -1, 0, 0};
-    for (int qq = 0; qq < (int) q.size(); qq++) {
-      auto [x, y] = q[qq];
-      debug(x, y);
-      for (int i = 0; i < 4; i++) {
-        int tx = x + dx[i];
-        int ty = y + dy[i];
-        if (tx < 0 || ty < 0 || tx >= n || ty >= m) {
-          continue;
-        }
-        if (st[tx][ty] || g[tx][ty] == '?') {
-          continue;
-        }
-        auto [zx, zy] = mp[g[tx][ty]];
-        if (x == zx + tx && y == zy + ty) {
-          st[tx][ty] = true;
-          q.emplace_back(make_pair(tx, ty));
-        }
-      }
-    }
-    /*
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        cout << st[i][j] << " \n"[j == m - 1];
-      }
-    }
-    */
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        if (g[i][j] == '?') {
-          bool ok = false;
-          for (int k = 0; k < 4; k++) {
-            int ti = i + dx[k];
-            int tj = j + dy[k];
-            if (ti < 0 || tj < 0 || ti >= n || tj >= m) {
-              continue;
-            }
-            ok |= (!st[ti][tj]);
-          }
-          if (!ok) {
-            st[i][j] = true;
-          }
-        }
-      }
-    }
-    int ans = n * m;
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        if (st[i][j]) {
-          ans -= 1;
-        }
-      }
-    }
-    cout << ans << '\n';
+  int n, m;
+  cin >> n >> m;
+  vector<vector<int>> g(n);
+  vector<pair<int, int>> edges(m);
+  for (int i = 0; i < m; i++) {
+    int a, b;
+    cin >> a >> b;
+    --a;
+    --b;
+    edges[i] = make_pair(a, b);
+    g[a].emplace_back(b);
+    g[b].emplace_back(a);
   }
+  vector<int> s(n, -1);
+  bool flag = true;
+  auto Dfs = [&](auto& self, int u, int c) -> void {
+    s[u] = c;
+    for (auto& v : g[u]) {
+      if (s[v] == -1) {
+        self(self, v, c ^ 1);
+        continue;
+      }
+      flag &= ((c ^ 1) == s[v]);
+    }
+  };
+  Dfs(Dfs, 0, 0);
+  if (!flag) {
+    cout << "NO\n";
+    return 0;
+  }
+  string ans(m, '#');
+  for (int i = 0; i < m; i++) {
+    auto [a, b] = edges[i];
+    ans[i] = (char) ('0' + s[a]);
+  }
+  cout << "YES\n";
+  cout << ans << '\n';
   return 0;
 }
