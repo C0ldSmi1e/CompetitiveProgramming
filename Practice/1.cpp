1fd21c16b3b9a95a706c3387a65fac7867343376
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2025-04-01 22:31:38 -0700

    update

--- Practice/1.cpp
+++ Practice/1.cpp
@@ -1,8 +1,3 @@
-/**
- * Author: C0ldSmi1e
- * Created Time: 03/20/2025 07:54:49 AM
-**/
-
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -17,24 +12,5 @@ int main() {
   cin.tie(nullptr)->sync_with_stdio(false);
   cout << fixed << setprecision(10);
 
-  int n;
-  cin >> n;
-  vector<int> a(n);
-  for (auto& u : a) {
-    cin >> u;
-  }
-  vector<int> b(n);
-  for (int i = 0; i < n; i++) {
-    b[i] = (a[i] - i + n - 1) / n;
-  }
-  int mn = (int) 1e9;
-  int mni = n;
-  for (int i = 0; i < n; i++) {
-    if (b[i] < mn) {
-      mn = b[i];
-      mni = i;
-    }
-  }
-  cout << mni + 1 << '\n';
   return 0;
 }
--- Practice/3.cpp
+++ Practice/3.cpp
@@ -1,6 +1,6 @@
 /**
  * Author: C0ldSmi1e
- * Created Time: 03/11/2025 07:31:50 AM
+ * Created Time: 03/25/2025 07:39:15 AM
 **/
 
 #include <bits/stdc++.h>
@@ -17,68 +17,34 @@ int main() {
   cin.tie(nullptr)->sync_with_stdio(false);
   cout << fixed << setprecision(10);
 
-  int T;
-  cin >> T;
-  while (T--) {
-    int n, k;
-    cin >> n >> k;
-    vector<int> c(n);
-    for (auto& u : c) {
-      cin >> u;
-    }
-    vector<bool> st(n);
-    for (int i = 0; i < k; i++) {
-      int x;
-      cin >> x;
-      --x;
-      st[x] = true;
-    }
-    vector<vector<int>> g(n);
-    vector<int> din(n);
-    for (int i = 0; i < n; i++) {
-      int m;
-      cin >> m;
-      vector<int> b(m);
-      for (int j = 0; j < m; j++) {
-        int x;
-        cin >> x;
-        --x;
-        g[x].emplace_back(i);
-        din[i] += 1;
+  long long n, b;
+  cin >> n >> b;
+  map<long long, long long> mp;
+  for (long long i = 2; i <= b / i; i++) {
+    if (b % i == 0) {
+      while (b % i == 0) {
+        b /= i;
+        mp[i] += 1;
       }
-    }
-    vector<int> q;
-    const long long INF = (long long) 1e18;
-    vector<long long> ans(n, INF);
-    for (int i = 0; i < n; i++) {
-      if (!din[i]) {
-        if (st[i]) {
-          ans[i] = 0;
-        } else {
-          ans[i] = c[i];
-        }
-        q.emplace_back(i);
-      }
-    }
-    for (int qq = 0; qq < (int) q.size(); qq++) {
-      int u = q[qq];
-      for (auto& v : g[u]) {
-        if (ans[v] == INF) {
-          ans[v] = 0;
-        }
-        ans[v] += ans[u];
-        if (--din[v] == 0) {
-          q.emplace_back(v);
-          ans[v] = min(ans[v], 1ll * c[v]);
-          if (st[v]) {
-            ans[v] = 0;
-          }
-        }
-      }
-    }
-    for (int i = 0; i < n; i++) {
-      cout << ans[i] << " \n"[i == n - 1];
     }
   }
+  if (b > 1) {
+    mp[b] = 1;
+  }
+  long long mn = (long long) 1e18;
+  for (auto& [x, c] : mp) {
+    long long z = x;
+    long long cnt = 0;
+    while (true) {
+      cnt += n / z;
+      if (z <= n / x) {
+        z *= x;
+        continue;
+      }
+      break;
+    }
+    mn = min(mn, cnt / c);
+  }
+  cout << mn << '\n';
   return 0;
 }
