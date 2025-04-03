1122b78cd59666412d296fb5a7a6f4dae8e90641
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2025-04-02 21:16:31 -0700

    update

--- Practice/1.cpp
+++ Practice/1.cpp
@@ -12,5 +12,25 @@ int main() {
   cin.tie(nullptr)->sync_with_stdio(false);
   cout << fixed << setprecision(10);
 
+  int T;
+  cin >> T;
+  while (T--) {
+    multiset<int> S{0, 3, 0, 1, 2, 0, 2, 5};
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    int ans = -1;
+    for (int i = 0; i < n; i++) {
+      int x;
+      cin >> x;
+      if (S.count(x)) {
+        S.erase(S.find(x));
+        if ((int) S.size() == 0) {
+          ans = i;
+        }
+      }
+    }
+    cout << ans + 1 << '\n';
+  }
   return 0;
 }
