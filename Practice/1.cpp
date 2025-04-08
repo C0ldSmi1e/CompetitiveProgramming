adf4e9f5cff307982c7bf1d016a2411ab09e2bfb
C0ldSmi1e <DanielYu3790@gmail.com>
2025-04-07 21:15:37 -0700

    update

--- Practice/1.cpp
+++ Practice/1.cpp
@@ -1,6 +1,6 @@
 /**
  * Author: C0ldSmi1e
- * Created Time: 04/06/2025 07:38:02 AM
+ * Created Time: 04/07/2025 09:07:31 PM
 **/
 
 #include <bits/stdc++.h>
@@ -17,66 +17,31 @@ int main() {
   cin.tie(nullptr)->sync_with_stdio(false);
   cout << fixed << setprecision(10);
 
-  string s = "What are you doing at the end of the world? Are you busy? Will you save us?";
-  const int N = 100010;
-  const long long INF = (long long) 1e18;
-  vector<long long> len(N);
-  len[0] = (int) s.size();
-  string l = "What are you doing while sending \"";
-  string m = "\"? Are you busy? Will you send \"";
-  string r = "\"?";
-  long long bad = -1;
-  for (int i = 1; i < N; i++) {
-    if (bad != -1) {
-      len[i] = bad;
-      continue;
-    }
-    len[i] = 2 * len[i - 1] + (int) l.size() + (int) m.size() + (int) r.size();
-    if (len[i] >= INF) {
-      bad = len[i];
-    }
-  }
-  auto Dfs = [&](auto& self, int n, long long k) -> char {
-    if (n == 0) {
-      return s[k];
-    }
-    long long t = (len[n] - (int) l.size() - (int) m.size() - (int) r.size()) / 2;
-    // l
-    if (k < (int) l.size()) {
-      return l[k];
-    }
-    // the first t
-    if (k < (int) l.size() + t) {
-      k -= (int) l.size();
-      return self(self, n - 1, k);
-    }
-    // m
-    if (k < (int) l.size() + t + (int) m.size()) {
-      k -= (int) l.size() + t;
-      return m[k];
-    }
-    // the second t
-    if (k < (int) l.size() + t + (int) m.size() + t) {
-      k -= (int) l.size() + t + (int) m.size();
-      return self(self, n - 1, k);
-    }
-    // r
-    k -= (int) l.size() + t + (int) m.size() + t;
-    return r[k];
-  };
-  // l + t + m + t + r
   int T;
   cin >> T;
   while (T--) {
-    int n;
-    long long k;
-    cin >> n >> k;
-    if (k > len[n]) {
-      cout << '.';
-      continue;
+    int n, x;
+    cin >> n >> x;
+    vector<int> a(n);
+    for (auto& u : a) {
+      cin >> u;
     }
-    k -= 1;
-    cout << Dfs(Dfs, n, k);
+    sort(a.rbegin(), a.rend());
+    int ans = 0;
+    debug(a);
+    for (int i = 0; i < n; i++) {
+      long long cur = 0;
+      int j = i;
+      while (j < n && cur < x) {
+        cur = 1ll * (j - i + 1) * a[j];
+        j += 1;
+      }
+      if (cur >= x) {
+        ans += 1;
+      }
+      i = j - 1;
+    }
+    cout << ans << '\n';
   }
   return 0;
 }
