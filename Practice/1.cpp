15d566627ad7039f742f9c31890321eedeba05a8
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2025-07-12 22:51:58 -0700

    update

--- Practice/1.cpp
+++ Practice/1.cpp
@@ -1,6 +1,6 @@
 /**
  * Author: C0ldSmi1e
- * Created Time: 07/09/2025 10:20:16 PM
+ * Created Time: 07/12/2025 10:38:36 PM
 **/
 
 #include <bits/stdc++.h>
@@ -17,32 +17,5 @@ int main() {
   cin.tie(nullptr)->sync_with_stdio(false);
   cout << fixed << setprecision(10);
 
-  int T;
-  cin >> T;
-  while (T--) {
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for (auto& u : a) {
-      cin >> u;
-    }
-    if (is_sorted(a.begin(), a.end())) {
-      cout << "NO\n";
-      continue;
-    }
-    int x = -1;
-    int y = -1;
-    for (int i = 0; i < n; i++) {
-      for (int j = i; j < n && x == -1; j++) {
-        if (a[i] > a[j]) {
-          x = a[i];
-          y = a[j];
-        }
-      }
-    }
-    cout << "YES\n";
-    cout << "2\n";
-    cout << x << ' ' << y << '\n';
-  }
   return 0;
 }
