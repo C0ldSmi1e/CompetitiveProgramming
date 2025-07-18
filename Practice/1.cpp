087357aa351401d7bca1d32ce226381e51c4681a
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2025-07-17 23:53:07 -0700

    update

--- Practice/1.cpp
+++ Practice/1.cpp
@@ -1,6 +1,6 @@
 /**
  * Author: C0ldSmi1e
- * Created Time: 07/12/2025 10:38:36 PM
+ * Created Time: 07/17/2025 11:42:52 PM
 **/
 
 #include <bits/stdc++.h>
@@ -17,5 +17,25 @@ int main() {
   cin.tie(nullptr)->sync_with_stdio(false);
   cout << fixed << setprecision(10);
 
+  int T;
+  cin >> T;
+  while (T--) {
+    int a, b;
+    cin >> a >> b;
+    if (a * 2 < b) {
+      cout << "0\n";
+      continue;
+    }
+    int l = 0, r = a;
+    while (l < r) {
+      int mid = (l + r) >> 1;
+      if (a - mid < b - 2 * mid) {
+        l = mid + 1;
+      } else {
+        r = mid;
+      }
+    }
+    cout << a - r << '\n';
+  }
   return 0;
 }
