5c4e946dc4d265d774c20eefef18d15f2afef6b4
C0ldSmi1e <DanielYu3790@gmail.com>
2025-07-28 11:23:25 -0700

    update

--- Practice/1.cpp
+++ Practice/1.cpp
@@ -1,6 +1,6 @@
 /**
  * Author: C0ldSmi1e
- * Created Time: 05/15/2025 10:52:04 PM
+ * Created Time: 07/28/2025 11:22:15 AM
 **/
 
 #include <bits/stdc++.h>
@@ -17,29 +17,5 @@ int main() {
   cin.tie(nullptr)->sync_with_stdio(false);
   cout << fixed << setprecision(10);
 
-  int T;
-  cin >> T;
-  while (T--) {
-    int n;
-    cin >> n;
-    if (n < 5) {
-      cout << "-1\n";
-      continue;
-    }
-    for (int i = 1; i <= n; i += 2) {
-      if (i == 5) {
-        continue;
-      }
-      cout << i << ' ';
-    }
-    cout << "5 4 ";
-    for (int i = 2; i <= n; i += 2) {
-      if (i == 4) {
-        continue;
-      }
-      cout << i << ' ';
-    }
-    cout << '\n';
-  }
   return 0;
 }
--- Practice/2.cpp
+++ Practice/2.cpp
@@ -1,6 +1,6 @@
 /**
  * Author: C0ldSmi1e
- * Created Time: 04/30/2025 11:25:36 PM
+ * Created Time: 05/31/2025 05:38:39 AM
 **/
 
 #include <bits/stdc++.h>
@@ -13,21 +13,51 @@ using namespace std;
 #define debug(...) 42
 #endif
 
+template <typename T, typename U>
+vector<T> Dijkstra(const vector<vector<pair<int, U>>>& g, int start, const T INF) {
+  int n = static_cast<int>(g.size());
+  assert(start >= 0 && start < n);
+  vector<T> dist(n, INF);
+  dist[start] = 0;
+  priority_queue<pair<T, int>, vector<pair<T, int>>, greater<pair<T, int>>> heap;
+  heap.emplace(dist[start], start);
+  debug(dist);
+  vector<bool> st(n);
+  while ((int) heap.size() > 0) {
+    auto [d, u] = heap.top();
+    heap.pop();
+    if (dist[u] != d || st[u]) {
+      continue;
+    }
+    st[u] = true;
+    for (auto& [v, w] : g[u]) {
+      if (dist[v] > w | dist[u]) {
+        dist[v] = w | dist[u];
+        heap.emplace(dist[v], v);
+      }
+    }
+  }
+  // returns INF if there's no path
+  return dist;
+}
+
 int main() {
   cin.tie(nullptr)->sync_with_stdio(false);
   cout << fixed << setprecision(10);
 
-  int n, s;
-  cin >> n >> s;
-  if ((n == 1 && s == 1) || s <= 2 * (n - 1)) {
-    cout << "NO\n";
-    return 0;
+  int n, m;
+  cin >> n >> m;
+  vector<vector<pair<int, int>>> g(n);
+  for (int i = 0; i < m; i++) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    --a;
+    --b;
+    g[a].emplace_back(b, c);
+    g[b].emplace_back(a, c);
   }
-  cout << "YES\n";
-  for (int i = 0; i < n - 1; i++) {
-    cout << "1 ";
-  }
-  cout << s - (n - 1) << '\n';
-  cout << n << '\n';
+  const int INF = (int) 2e9;
+  auto d = Dijkstra(g, 0, INF);
+  cout << d.back() << '\n';
   return 0;
 }
