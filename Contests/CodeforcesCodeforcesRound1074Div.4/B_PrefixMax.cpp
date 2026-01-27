6097689592dfc7d9bc675f3d281995042ba51bb3
C0ldSmi1e <DanielYu3790@gmail.com>
2026-01-26 21:36:37 -0800

    update

--- Contests/CodeforcesCodeforcesRound1074Div.4/B_PrefixMax.cpp
+++ Contests/CodeforcesCodeforcesRound1074Div.4/B_PrefixMax.cpp
@@ -1,3 +1,8 @@
+/**
+ * Author: C0ldSmi1e
+ * Created Time: 01/26/2026 09:29:41 PM
+**/
+
 // time-limit: 1500
 // problem-url: https://codeforces.com/contest/2185/problem/B
 #include <bits/stdc++.h>
@@ -14,5 +19,17 @@ int main() {
   cin.tie(nullptr)->sync_with_stdio(false);
   cout << fixed << setprecision(10);
 
+  int T;
+  cin >> T;
+  while (T--) {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto& u : a) {
+      cin >> u;
+    }
+    int mx = *max_element(a.begin(), a.end());
+    cout << mx * n << '\n';
+  }
   return 0;
 }
