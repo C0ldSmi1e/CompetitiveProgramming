06c0652ce5f57ea71f99f3684991a145d2fb55b7
C0ldSmi1e <DanielYu3790@gmail.com>
2026-01-27 22:14:38 -0800

    update

--- Contests/CodeforcesCodeforcesRound1074Div.4/C_ShiftedMEX.cpp
+++ Contests/CodeforcesCodeforcesRound1074Div.4/C_ShiftedMEX.cpp
@@ -1,3 +1,8 @@
+/**
+ * Author: C0ldSmi1e
+ * Created Time: 01/26/2026 09:37:33 PM
+**/
+
 // time-limit: 2000
 // problem-url: https://codeforces.com/contest/2185/problem/C
 #include <bits/stdc++.h>
@@ -14,5 +19,31 @@ int main() {
   cin.tie(nullptr)->sync_with_stdio(false);
   cout << fixed << setprecision(10);
 
+  int T;
+  cin >> T;
+  while (T--) {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto& u : a) {
+      cin >> u;
+    }
+    sort(a.begin(), a.end());
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+      int j = i + 1;
+      int len = 1;
+      while (j < n && a[j] - a[j - 1] <= 1) {
+        if (a[j] > a[j - 1]) {
+          len += 1;
+        }
+        j += 1;
+      }
+      // debug(i, j, len);
+      ans = max(ans, len);
+      i = j - 1;
+    }
+    cout << ans << '\n';
+  }
   return 0;
 }
--- Contests/CodeforcesCodeforcesRound1074Div.4/D_OutOfMemoryError.cpp
+++ Contests/CodeforcesCodeforcesRound1074Div.4/D_OutOfMemoryError.cpp
@@ -1,3 +1,8 @@
+/**
+ * Author: C0ldSmi1e
+ * Created Time: 01/26/2026 09:57:55 PM
+**/
+
 // time-limit: 2000
 // problem-url: https://codeforces.com/contest/2185/problem/D
 #include <bits/stdc++.h>
@@ -14,5 +19,44 @@ int main() {
   cin.tie(nullptr)->sync_with_stdio(false);
   cout << fixed << setprecision(10);
 
+  int T;
+  cin >> T;
+  while (T--) {
+    int n, m, h;
+    cin >> n >> m >> h;
+    vector<int> a(n);
+    for (auto& u : a) {
+      cin >> u;
+    }
+    vector<pair<int, int>> ops(m);
+    for (auto& [x, y] : ops) {
+      cin >> x >> y;
+      --x;
+    }
+    auto c = a;
+    vector<int> b(n);
+    int cnt = 0;
+    for (auto& [x, y] : ops) {
+      if (b[x] != cnt) {
+        b[x] = cnt;
+        c[x] = a[x];
+      }
+      c[x] += y;
+      if (c[x] > h) {
+        cnt += 1;
+        b[x] = cnt;
+        c[x] = a[x];
+      }
+    }
+    for (int i = 0; i < n; i++) {
+      if (b[i] != cnt) {
+        c[i] = a[i];
+      }
+    }
+    for (auto& u : c) {
+      cout << u << ' ';
+    }
+    cout << '\n';
+  }
   return 0;
 }
