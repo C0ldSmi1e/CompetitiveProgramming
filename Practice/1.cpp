d23a3e8ecf1d5254627cc3b207bf26f47e51fbf9
C0ldSmi1e <DanielYu3790@gmail.com>
2026-09-02 10:08:32 -0700

    fix

--- Practice/1.cpp
+++ Practice/1.cpp
@@ -8,6 +8,34 @@ using namespace std;
 #define debug(...) 42
 #endif
 
+class Dsu {
+ public:
+  vector<int> p, sz;
+  int n;
+  Dsu(int _n) {
+    n = _n + 1;
+    p.resize(n);
+    sz.assign(n, 1);
+    iota(p.begin(), p.end(), 0);
+  }
+  inline int Get(int x) {
+    return (x == p[x] ? x : (p[x] = Get(p[x])));
+  }
+  inline bool Unite(int x, int y) {
+    x = Get(x);
+    y = Get(y);
+    if (x != y) {
+      p[x] = y;
+      sz[y] += sz[x];
+      return true;
+    }
+    return false;
+  }
+  inline int GetSize(int x) {
+    return sz[Get(x)];
+  }
+};
+
 int main() {
   cin.tie(nullptr)->sync_with_stdio(false);
   cout << fixed << setprecision(10);
--- Template/DataStructure/Fenwick.cpp
+++ Template/DataStructure/Fenwick.cpp
@@ -614,7 +614,7 @@ class Fenwick {
   }
   // ask for [x1, x2] and [y1, y2]
   inline T get(int x1, int y1, int x2, int y2) {
-    assert (x1 <= x2 && y1 <= y1);
+    assert (x1 <= x2 && y1 <= y2);
     assert(x1 >= 0 && x1 < n && x2 >= 0 && x2 < n);
     assert(y1 >= 0 && y1 < m && y2 >= 0 && y2 < m);
     T res = get(x2, y2);
